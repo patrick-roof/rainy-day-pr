047abb834dbd8609bdca61f515207b8cd00ff517
patrick <roosterruf@gmail.com>
2022-12-05 21:53:16 -0700

    JS almost fully functional, doesn't create buttons

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ var day5 = document.getElementById('day5');
 
 var daysArray = [day1,day2,day3,day4,day5];
 
+var searchHistory = [];
+
 let weather = {
     apiKey: 'd9d9c2b250611e67725494f9e6abec68',
     fetchWeather: function (city) {
@@ -58,13 +60,14 @@ let weather = {
 document.querySelector('.searchBtn').addEventListener('click', function () {
     weather.search();
     forecast.search();
+    addSearchHistory();
 })
 
 let forecast = {
     apiKey: 'd9d9c2b250611e67725494f9e6abec68',
     fetchForecast: function (city) {
         fetch(
-            "api.openweathermap.org/data/2.5/forecast?q=" 
+            "https://api.openweathermap.org/data/2.5/forecast?q=" 
             + city
              + "&units=imperial&appid="
               + this.apiKey
@@ -73,25 +76,41 @@ let forecast = {
             .then((response) => response.json())
             .then((data) => this.displayForecast(data));
     },
-    displayForecast: function(data) {
-
-        const { name } = data
-        const { icon } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        console.log(name,icon,temp,humidity,speed);
-        document.querySelector(".forecast-city").innerText = 'Weather in ' + name + ' - ';
-        document.querySelector(".forecast-icon").src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
-        document.querySelector(".forecast-temp").innerText = temp + '°F';
-        document.querySelector(".forecast-humidity").innerText = 'Humidity: ' + humidity + '%';
-        document.querySelector(".forecast-wind-speed").innerText = 'Wind: ' + speed + 'mph';
+    displayForecast: function(dataForecast) {
+        console.log(dataForecast);
+        let index = 1;
+        for (let i=0; i < dataForecast.list.length; i++) {
+            if (dataForecast.list[i].dt_txt.indexOf('12:00:00') > 0 ) {
+                let data = dataForecast.list[i]
+                const { icon } = data.weather[0];
+                const { temp, humidity } = data.main;
+                const { speed } = data.wind;
+                console.log(icon,temp,humidity,speed);
+                document.querySelector("#day" + index + " .forecast-icon").src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+                document.querySelector("#day" + index + " .forecast-temp").innerText = temp + '°F';
+                document.querySelector("#day" + index + " .forecast-humidity").innerText = 'Humidity: ' + humidity + '%';
+                document.querySelector("#day" + index + " .forecast-wind-speed").innerText = 'Wind: ' + speed + 'mph';
+                index += 1
+            }
+        }
     },
     search: function () {
         this.fetchForecast(document.querySelector('.search-bar').value);
     }
 };
 
-for (var i = 0; i < daysArray.length; i ++) {
-        fetchForecast();
-        displayForecast();
+function addSearchHistory() {
+    var weatherLog = document.querySelector('.search-bar').value;
+    console.log(weatherLog);
+    searchHistory.push(weatherLog)
+    localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    createSearch()
+}
+
+function createSearch() {
+    localStorage.getItem('search-history', JSON.parse(searchHistory));
+    var searchedItem = ('search-history');
+    var searchHistoryEl = $('<button class="searchResult">');
+    searchHistoryEl.text(searchBar);
+    searchedItem.append(searchHistoryEl);
 }
